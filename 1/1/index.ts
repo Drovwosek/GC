d2688c1fa4b11ff5d7f7b77ad0a98ea6e1f9fe54
Drovwosek <ftr5wa2@gmail.com>
2024-03-17 19:36:59 +0300

    Написал 1 лабу 1 задание. Статичные буквы. Работает

--- 1/1/index.ts
+++ 1/1/index.ts
@@ -4,16 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
 )
 
 function start() {
-    const canvas = document.getElementsByTagName('canvas') [0]
-    createA('black')
-    createA('black')
-    createA('black')
+    const canvas = document.getElementsByTagName('canvas')[0]
+    const ctx = canvas.getContext('2d')
+
+    const a1 = createA('black')
+    const a2 = createA('black')
+    const a3 = createA('black')
 
 }
 
 function createA(color: string = 'blue'): HTMLCanvasElement {
-    console.log("creating A")
     const canvas = document.createElement('canvas')
+    document.body.appendChild(canvas)
     canvas.width = 60
     canvas.height = 100
     const ctx = canvas.getContext('2d')
@@ -23,7 +25,7 @@ function createA(color: string = 'blue'): HTMLCanvasElement {
     return canvas
 }
 
-function drawA(ctx: CanvasRenderingContext2D, x: number, y: number, color: string = 'blue'): void {
+function drawA(ctx: CanvasRenderingContext2D, x: number, y: number, color: string = 'black'): void {
     const a = new Path2D()
 
     a.moveTo(x, y + 100)
@@ -33,13 +35,15 @@ function drawA(ctx: CanvasRenderingContext2D, x: number, y: number, color: strin
 
     a.lineTo(x + 50, y + 100)
     a.lineTo(x + 35, y + 34)
+
+    a.lineTo(x + 30, y + 14)
     a.lineTo(x + 25, y + 34)
     a.lineTo(x + 10, y + 100)
-    a.lineTo(x, y + 100)
+    ctx.fillRect(x + 15, y + 70, 30, 10)
 
-    ctx.fillStyle = color
     ctx.fill(a)
 
-    ctx.fillRect(x + 15, y + 70, 30, 10)
-    console.log("draw A")
+
 }
+
+
