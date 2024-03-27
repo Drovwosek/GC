f212cf11ebed59424c0dc351ba235623597fb601
Drovwosek <ftr5wa2@gmail.com>
2024-03-28 01:32:45 +0300

    Отмасштабировал

--- 1/2/index.ts
+++ 1/2/index.ts
@@ -1,80 +1,79 @@
-
 document.addEventListener('DOMContentLoaded', () => {
         drawHouse()
     }
 )
-//TODO: аче как мелко? Увеличь масштаб
+
 function drawHouse()  {
     const canvas = document.getElementsByTagName('canvas')[0]
-    canvas.width = 220
-    canvas.height = 140
+    canvas.width = 440
+    canvas.height = 280
 
     const ctx = canvas.getContext('2d')
 
     drawWalls(ctx)
     drawPipe(ctx)
-    drawDoor(ctx)
     drawRoof(ctx)
-    drawWindow(ctx, 65, 70)
-    drawWindow(ctx, 125, 70)
+    drawDoor(ctx)
+    drawWindow(ctx, 130, 150)
+    drawWindow(ctx, 250, 150)
     drawFence(ctx)
 }
 
 function drawWalls(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = '#C09A6E'
-    ctx.fillRect(60, 60, 100, 60)
+    ctx.fillRect(120, 120, 200, 140)
 }
 
 function drawPipe(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = '#AF5043'
-    ctx.fillRect(140, 0, 20, 50)
-}
-
-function drawDoor(ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle = '#A8855D'//TODO: почему не прорисовалась дверь?
-    ctx.fillRect(90, 120, 40, 3)
-
-    ctx.fillStyle = '#725B48'
-    ctx.fillRect(100, 90, 20, 30)
-
-    ctx.beginPath()
-    ctx.arc(103, 108, 2, 0, Math.PI * 2, true)
-    ctx.fillStyle = '#A8855D'
-    ctx.fill()
+    ctx.fillRect(280, 20, 40, 100)
 }
 
 function drawRoof(ctx: CanvasRenderingContext2D){
     const roof = new Path2D()
-    roof.moveTo(40, 60)
-    roof.lineTo(110, 10)
-    roof.lineTo(180, 60)
-    roof.lineTo(40, 60)
+    roof.moveTo(80, 120)
+    roof.lineTo(220, 20)
+    roof.lineTo(360, 120)
+    roof.lineTo(80, 120)
     ctx.fillStyle = '#725B48'
     ctx.fill(roof)
 }
 
+function drawDoor(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = '#A8855D'
+    ctx.fillRect(180, 256, 80, 6)
+
+    ctx.fillStyle = '#725B48'
+    ctx.fillRect(200, 196, 40, 60)
+
+    ctx.beginPath()
+    ctx.arc(206, 226, 4, 0, Math.PI * 2, false)
+    ctx.fillStyle = '#A8855D'
+    ctx.fill()
+}
+
 function drawWindow(ctx: CanvasRenderingContext2D, x: number, y: number) {
     ctx.fillStyle = '#EED73C'
-    ctx.fillRect(x, y, 30, 30)
+    ctx.fillRect(x, y, 60, 60)
 
     ctx.strokeStyle = '#725B48'
-    ctx.strokeRect(x, y, 30, 30)
+    ctx.strokeRect(x, y, 60, 60)
     ctx.beginPath()
-    ctx.moveTo(x, y + 15)
-    ctx.lineTo(x + 30, y + 15)
-    ctx.moveTo(x + 15, y)
-    ctx.lineTo(x + 15, y + 15)
+    ctx.moveTo(x, y + 30)
+    ctx.lineTo(x + 60, y + 30)
+    ctx.moveTo(x + 30, y)
+    ctx.lineTo(x + 30, y + 30)
     ctx.stroke()
 }
 
 function drawFence(ctx: CanvasRenderingContext2D) {
     for (let i = 0; i < 6; i++){
-        drawPlank(ctx, i * 15, 90)
-        drawPlank(ctx, 130 + i * 15, 90)
+        drawPlank(ctx, i * 30, 200)
+        drawPlank(ctx, 260 + i * 30, 200)
     }
 }
 
 function drawPlank(ctx: CanvasRenderingContext2D, x: number, y: number) {
     ctx.fillStyle = '#673b2f'
-    ctx.fillRect(x, y, 10, 50)
+    ctx.fillRect(x, y, 20, 100)
 }
